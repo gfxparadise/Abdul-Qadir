914a5721398bd8f807c04369fe187f50629d3930
gfxparadise <abdul.qadir.avadia@gmail.com>
2022-09-23 17:07:47 +0500

    now ready

---
+++ js/script.js
@@ -0,0 +1,72 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) => {
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+document.querySelectorAll('a').forEach(links => {
+
+    links.onmouseenter = () => {
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () => {
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+});
+
+// typing animation
+var typed = new Typed(".typing", {
+    strings: ["", "Graphic Designer", "Youtuber", "Video Editor"],
+    typespeed: 100,
+    backspeed: 60,
+    loop: true
+})
+
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () => {
+    toggleBtn.classList.replace('fa-sun', 'fa-moon');
+    body.classList.add('dark');
+    localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () => {
+    toggleBtn.classList.replace('fa-moon', 'fa-sun');
+    body.classList.remove('dark');
+    localStorage.setItem('dark-mode', 'disabled');
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+toggleBtn.onclick = (e) => {
+    darkMode = localStorage.getItem('dark-mode');
+    if (darkMode === 'disabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+}
