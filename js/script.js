b9c805a5a5d546ef685dd5aa91e67f21c70958fe
gfxparadise <abdul.qadir.avadia@gmail.com>
2022-10-07 17:35:44 +0500

    6

--- js/script.js
+++ js/script.js
@@ -98,33 +98,33 @@ valueDisplays.forEach((valueDisplay) => {
 
 // Email sender
 
-var form = document.getElementById("my-form");
+// var form = document.getElementById("my-form");
     
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/'
-        }
-      }).then(response => {
-        if (response.ok) {
-         alert='message has been send to <b> GFX Paradise <b>'
-          form.reset()
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status.innerHTML = "Oops! There was a problem submitting your form"
-            }
-          })
-        }
-      }).catch(error => {
-        alert= 'Oops! There was a problem submitting your form'
-      });
-    }
-    form.addEventListener("submit", handleSubmit)
+//     async function handleSubmit(event) {
+//       event.preventDefault();
+//       var status = document.getElementById("my-form-status");
+//       var data = new FormData(event.target);
+//       fetch(event.target.action, {
+//         method: form.method,
+//         body: data,
+//         headers: {
+//             'Accept': 'application/'
+//         }
+//       }).then(response => {
+//         if (response.ok) {
+//          alert='message has been send to <b> GFX Paradise <b>'
+//           form.reset()
+//         } else {
+//           response.json().then(data => {
+//             if (Object.hasOwn(data, 'errors')) {
+//               status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+//             } else {
+//               status.innerHTML = "Oops! There was a problem submitting your form"
+//             }
+//           })
+//         }
+//       }).catch(error => {
+//         alert= 'Oops! There was a problem submitting your form'
+//       });
+//     }
+//     form.addEventListener("submit", handleSubmit)
