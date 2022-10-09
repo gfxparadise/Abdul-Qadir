f1e192451ec35d131ed6a7745db1b773277f7a4c
gfxparadise <113798295+gfxparadise@users.noreply.github.com>
2022-10-09 09:35:46 -0700

    6

--- js/script.js
+++ js/script.js
@@ -96,3 +96,30 @@ valueDisplays.forEach((valueDisplay) => {
   }, duration);
 });
 
+function validate(){
+    let form = document.getElementById('my-form');
+    let email = document.getElementById('email').value;
+    let text = document.getElementById('text');
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(email.match(pattern))
+    {
+        form.classList.add('valid');
+        form.classList.remove('invalid');
+    text.innerHTML = "Your Email Adderss is Valid.";
+    text.style.color = "#4caf50";
+    }
+    else
+    {
+        form.classList.remove('valid');
+        form.classList.add('invalid');
+        text.innerHTML = "Please Enter Valid Email Address";
+        text.style.color = "#f44336";
+    }
+    if (email == "")
+    {
+        form.classList.remove('valid')
+        form.classList.remove('invalid');
+        text.innerHTML = "";
+    }
+}
